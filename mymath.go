211432a0abbc05c2f6329e7d56856349e6e06aa3
Vadim <flest@inbox.ru>
2024-07-09 16:40:27 +0500

    mymath

--- mymath.go
+++ mymath.go
@@ -36,9 +36,11 @@ func Yn(x int, y float64) float64 {
 func Acos(x float64) float64 {
 	return math.Acos(x)
 }
+
 func Acosh(x float64) float64 {
 	return math.Acosh(x)
 }
+
 func Asin(x float64) float64 {
 	return math.Asin(x)
 }
